0228e93fffef4176c5adce7b0d1cd4f39e5cfb44
Sam Justice <sam.justice1@gmail.com>
2016-04-23 12:17:42 -0400

    no message

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
 int main() {
 	
+	cout << "Running Floating Point Multiplication Emulator" << endl;
+	cout << "Sam Justice - CSIS 342-001, Spring 2016 - Floating Point Multiplication Emulator" << endl;
+	
+
+
 	return 0;
 }
